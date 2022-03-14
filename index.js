2ccb20412b69b0b28f777cc9e5286d2646fcb882
ninelamericanel <ninelamericanel@gmail.com>
2022-03-14 19:25:54 +0300

    created class View, block for search, input and method for create elements

--- index.js
+++ index.js
@@ -1,64 +1,76 @@
-function createElement(tag, className, parent, type, placeholder) {
-    let parentBlock = document.querySelector(parent);
-    let newElement = document.createElement(tag);
-    if (type) newElement.type = type;
-    if (className) newElement.className = className;
-    if(placeholder) newElement.placeholder = placeholder;
-    parentBlock.append(newElement);
+class View {
+    constructor() {
+        this.root = document.getElementById('root');
+        this.searchBlock = this.createElement('div', 'search');
+        this.root.append(this.searchBlock);
 
-    return document.querySelector(`.${className}`)
-}
+        this.searchInput = this.createElement('input', 'search__input');
+        this.searchBlock.append(this.searchInput);
 
-createElement('div', "search", '.root');
-createElement('input', "search__input", '.search', 'text', 'Type to search...');
 
-let input = document.querySelector('.search__input');
 
-input.addEventListener('input', () => {
-        let value = input.value;
-        if (value) {
-            getRepositories(value)
-                .then(result => createListOfAutocomplete(result.items));
-        } else {
-            createListOfAutocomplete();
-        }
     }
-);
 
-function createListOfAutocomplete(items) {
-    if (items) {
-        createElement('ul', 'search__autocomplete', '.search');
-        items.forEach((item, i) => {
-            let nodeElement = createElement('li', `search__item--${i}`, '.search__autocomplete')
-            nodeElement.innerHTML = item.name;
-        })
-    } else {
-        let list = document.querySelector('.search__autocomplete');
-        list.remove()
+    createElement(tag, className) {
+        let newElement = document.createElement(tag);
+        if (className) newElement.className = className;
+        return newElement;
     }
-}
-
-function createListOfAddedRepositories () {
 
 }
 
+new View();
 
-async function getRepositories(text) {
-    let response = await fetch(`https://api.github.com/search/repositories?q=${text}&per_page=5`);
-    if (response.ok) {
-        let json = await response.json();
-        return json;
-    } else {
-        return `Error ${response.status}`;
-    }
-}
-
-function debounce(fn, delay) {
-    let timer;
-    return function (...args) {
-        if (timer) clearTimeout(timer);
-        timer = setTimeout(fn.apply(this, args), delay);
-    }
-}
-
-
+//
+// createElement('div', "search", '.root');
+// createElement('input', "search__input", '.search', 'text', 'Type to search...');
+//
+//
+// input.addEventListener('input', () => {
+//         let value = input.value;
+//         if (value) {
+//             getRepositories(value)
+//                 .then(result => createListOfAutocomplete(result.items));
+//         } else {
+//             createListOfAutocomplete();
+//         }
+//     }
+// );
+//
+// function createListOfAutocomplete(items) {
+//     if (items) {
+//         createElement('ul', 'search__autocomplete', '.search');
+//         items.forEach((item, i) => {
+//             let nodeElement = createElement('li', `search__item--${i}`, '.search__autocomplete')
+//             nodeElement.innerHTML = item.name;
+//         })
+//     } else {
+//         let list = document.querySelector('.search__autocomplete');
+//         list.remove()
+//     }
+// }
+//
+// function createListOfAddedRepositories() {
+//
+// }
+//
+//
+// async function getRepositories(text) {
+//     let response = await fetch(`https://api.github.com/search/repositories?q=${text}&per_page=5`);
+//     if (response.ok) {
+//         let json = await response.json();
+//         return json;
+//     } else {
+//         return `Error ${response.status}`;
+//     }
+// }
+//
+// function debounce(fn, delay) {
+//     let timer;
+//     return function (...args) {
+//         if (timer) clearTimeout(timer);
+//         timer = setTimeout(fn.apply(this, args), delay);
+//     }
+// }
+//
+//
